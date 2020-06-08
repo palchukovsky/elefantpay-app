36bde1224dd9744ee4ecc92c37d61e8bca27a77e
Eugene Palchukovsky <eugene@palchukovsky.com>
2020-06-08 23:49:39 +0300

    Add link to contact from on the web-site

--- lib/help.dart
+++ lib/help.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-requestHelp() {}
+requestHelp() async {
+  const url = 'https://elefantpay.com/#contacts';
+  if (await canLaunch(url)) {
+    launch(url);
+  }
+}
 
 class HelpFloatingButton extends FloatingActionButton {
   HelpFloatingButton()
-      : super(
-            onPressed: requestHelp(), tooltip: 'Help', child: Icon(Icons.help));
+      : super(onPressed: requestHelp, tooltip: 'Help', child: Icon(Icons.help));
 }
